d2046c7b15e18158f14ce4297944c859406041f9
ImanX <imannamix@gmail.com>
2018-02-10 22:06:15 +0330

    commit

---
+++ Shahre Man/Controllers/ContactController.swift
@@ -0,0 +1,9 @@
+//
+//  ContactController.swift
+//  Shahre Man
+//
+//  Created by ImanX on 2/10/18.
+//  Copyright © 2018 ImanX. All rights reserved.
+//
+
+import Foundation
---
+++ Shahre Man/ViewControllers/ContactUsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContactUs.swift
+//  Shahre Man
+//
+//  Created by ImanX on 2/10/18.
+//  Copyright © 2018 ImanX. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ContactUsViewController : BaseViewController{
+    
+}
